5bc3804572f8e9729fe3967a74fa9651334bc91b
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2017-08-27 14:08:41 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+const {app, BrowserWindow, ipcMain} = electron;
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        console.log('Video duration is: ', metadata.format.duration);
+
+        if (err) {
+            console.log(err);
+        };
+
+    });
+});
